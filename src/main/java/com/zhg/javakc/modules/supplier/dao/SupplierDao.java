3fc0eb780505aa9a1187278fbd03049f952d6744
axd <axd1998@163.com>
2019-11-22 17:37:34 +0800

    axd

---
+++ src/main/java/com/zhg/javakc/modules/supplier/dao/SupplierDao.java
@@ -0,0 +1,11 @@
+package com.zhg.javakc.modules.supplier.dao;
+
+/**
+ * @author an
+ * @version v0.0.1
+ * @date 2019/11/21 20:31
+ */
+
+
+public interface SupplierDao {
+}
---
+++ src/main/java/com/zhg/javakc/modules/supplier/entity/SupplierEntity.java
@@ -0,0 +1,11 @@
+package com.zhg.javakc.modules.supplier.entity;
+
+/**
+ * @author an
+ * @version v0.0.1
+ * @date 2019/11/21 20:32
+ */
+
+
+public class SupplierEntity {
+}
