67b8219ce7af8527c2bb612041bca42030e1c0bc
axd <axd1998@163.com>
2019-11-23 12:49:55 +0800

    采购价模块

---
+++ src/main/java/com/zhg/javakc/modules/purchase/entity/PurchaseEntity.java
@@ -0,0 +1,17 @@
+package com.zhg.javakc.modules.purchase.entity;
+
+/**
+ * @author an
+ * @version v0.0.1
+ * @date 2019/11/22 20:14
+ */
+
+import javax.persistence.Entity;
+
+/**
+ * 采购价实体类
+ */
+
+@Entity
+public class PurchaseEntity {
+}
